46ca79708a21ff01694155787fc44c98f23719a0
lehakhorkov <Khorkovvv@mail.ru>
2023-08-14 21:54:39 +0300

    javascript(введение)

---
+++ ДЗ23(javascript-введение)/index.js
@@ -0,0 +1,5 @@
+document.querySelector('.one').innerHTML = "<b>Это камни. Верность над чернеющей Невою.</b>";
+
+document.querySelector('.twoo').innerHTML = "<b>Осень вновь напомнила душе о самом главном. Осень я опять лишен покоя.</b>";
+
+alert('ДДТ');
